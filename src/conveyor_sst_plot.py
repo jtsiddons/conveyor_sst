558c339f4c23559e6d91701fca59dc9376622a72
jtsiddons <jtsiddons@gmail.com>
2023-02-08 17:33:48 +0000

    Corrected midpoint function. Refactor code.

--- src/conveyor_sst_plot.py
+++ src/conveyor_sst_plot.py
@@ -11,27 +11,91 @@ from math import atan2, cos, sin, sqrt
 PROJECT_DIR = sys.prefix
 DATA_FILE = os.path.join(PROJECT_DIR, 'data', 'conveyor_sst.csv')
 OUTDIR = os.path.join(PROJECT_DIR, 'figs', 'sst_frames')
+YEARS = [str(y) for y in range(1982, 2022)]
+PATH_FILE = pl.read_csv(DATA_FILE)
+
+# Get differences since 1982
+PATH_SST = (
+    PATH_FILE
+    .with_columns([
+        (pl.col(y) - pl.col(YEARS[0])).alias(y) for y in YEARS[1:]
+    ] + [pl.lit(0).alias(YEARS[0])]
+    )
+)
+print(PATH_SST)
+MAXABS = max(
+    PATH_SST
+    .select([
+        pl.col(y).abs().alias(y) for y in YEARS
+    ])
+    .max()
+    .row(0)
+)
+NORM = plt.Normalize(-MAXABS, MAXABS)
+CMAP = plt.colormaps['RdBu_r']
+LWIDTH = 7
 
 
 # Need to think about midpoint generation
 def midpoint(lon0: int, lon1: int, lat0: int, lat1: int) -> Tuple[float, float]:
-    theta0 = np.deg2rad(lat0)
-    theta1 = np.deg2rad(lat1)
-    phi0 = np.deg2rad(lon0)
-    phi1 = np.deg2rad(lon1)
-
     dlon = np.deg2rad(lon1 - lon0)
 
-    dx = cos(theta1) * cos(dlon)
-    dy = cos(theta1) * sin(dlon)
+    lat0 = np.deg2rad(lat0)
+    lat1 = np.deg2rad(lat1)
+    lon0 = np.deg2rad(lon0)
+    # lon1 = np.deg2rad(lon1)
 
-    theta_mid = atan2(sin(theta0) + sin(theta1),
-                      sqrt((cos(theta0) + dx)*(cos(theta1) + dx) + dy**2))
-    phi_mid = phi0 + atan2(dy, cos(theta0) + dx)
-    return (np.rad2deg(phi_mid), np.rad2deg(theta_mid))
+    Bx = cos(lat1) * cos(dlon)
+    By = cos(lat1) * sin(dlon)
+    lat2 = atan2(sin(lat0) + sin(lat1), sqrt((cos(lat0)+Bx)**2 + By**2))
+    lon2 = lon0 + atan2(By, cos(lat0) + Bx)
+
+    return (np.rad2deg(lon2), np.rad2deg(lat2))
 
 
-def year_frame(year: str, path_sst: pl.DataFrame, maxabs: float):
+def draw_segment(path, year, i, ax):
+    num_path_points = len(path)
+    beforelon, beforelat = midpoint(
+        path['longitude'][(i-1) % num_path_points],
+        path['longitude'][(i) % num_path_points],
+        path['latitude'][(i-1) % num_path_points],
+        path['latitude'][(i) % num_path_points],
+    )
+    afterlon, afterlat = midpoint(
+        path['longitude'][(i+1) % num_path_points],
+        path['longitude'][(i) % num_path_points],
+        path['latitude'][(i+1) % num_path_points],
+        path['latitude'][(i) % num_path_points],
+    )
+    lons = [
+        beforelon,
+        path['longitude'][i],
+        afterlon,
+    ]
+    lats = [
+        beforelat,
+        path['latitude'][i],
+        afterlat,
+    ]
+    temp_diff = path[year][i]
+
+    if year == YEARS[0]:
+        print(lons)
+        print(lats)
+        print(temp_diff)
+        print('-'*20)
+        print('')
+
+    ax.plot(
+        lons,
+        lats,
+        linewidth=LWIDTH,
+        color=CMAP(NORM(temp_diff)),
+        transform=ccrs.Geodetic()
+    )
+
+
+def year_frame(year: str):
     plt.close()
     fig = plt.figure(figsize=(10, 5))
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.Robinson())
@@ -40,7 +104,7 @@ def year_frame(year: str, path_sst: pl.DataFrame, maxabs: float):
 
     # Draw background line
     for i in range(2):
-        path = path_sst.filter(pl.col('path') == i+1)
+        path = PATH_SST.filter(pl.col('path') == i+1)
         ax.plot(
             path['longitude'],
             path['latitude'],
@@ -49,13 +113,9 @@ def year_frame(year: str, path_sst: pl.DataFrame, maxabs: float):
             transform=ccrs.Geodetic()
         )
 
-    norm = plt.Normalize(-maxabs, maxabs)
-    cmap = plt.colormaps['RdBu_r']
-    lwidth = 7
-
     # Handle path 1
     # End points are not part of loop
-    path1 = path_sst.filter(pl.col('path') == 1)
+    path1 = PATH_SST.filter(pl.col('path') == 1)
 
     # Handle end-points
     _, lon0, lat0, *_ = path1.row(0)
@@ -68,89 +128,28 @@ def year_frame(year: str, path_sst: pl.DataFrame, maxabs: float):
     ax.plot(
         [lon0, lon01],
         [lat0, lat01],
-        linewidth=lwidth,
-        color=cmap(norm(path1[year][0])),
+        linewidth=LWIDTH,
+        color=CMAP(NORM(path1[year][0])),
         transform=ccrs.Geodetic(),
     )
     ax.plot(
         [lonend, lonendmid],
         [latend, latendmid],
-        linewidth=lwidth,
-        color=cmap(norm(path1[year][-1])),
+        linewidth=LWIDTH,
+        color=CMAP(NORM(path1[year][-1])),
         transform=ccrs.Geodetic(),
     )
 
     # Handle non-end points
     for i in range(1, len(path1)-1):
-        beforelon, beforelat = midpoint(
-            path1['longitude'][i-1],
-            path1['longitude'][i],
-            path1['latitude'][i-1],
-            path1['latitude'][i],
-        )
-        afterlon, afterlat = midpoint(
-            path1['longitude'][i+1],
-            path1['longitude'][i],
-            path1['latitude'][i+1],
-            path1['latitude'][i],
-        )
-        lons = [
-            beforelon,
-            path1['longitude'][i],
-            afterlon,
-        ]
-        lats = [
-            beforelat,
-            path1['latitude'][i],
-            afterlat,
-        ]
-        temp_diff = path1[year][i]
+        draw_segment(path1, year, i, ax)
 
-        ax.plot(
-            lons,
-            lats,
-            linewidth=8,
-            color=cmap(norm(path1[year][i])),
-            transform=ccrs.Geodetic()
-        )
+    # Handle Path 2 - Can use mod!
+    path2 = PATH_SST.filter(pl.col('path') == 2)
+    for i in range(len(path2)):
+        draw_segment(path2, year, i, ax)
 
-        # Handle Path 2 - Can use mod!
-    path2 = path_sst.filter(pl.col('path') == 2)
-    l = len(path2)
-    for i in range(l):
-        beforelon, beforelat = midpoint(
-            path2['longitude'][(i-1) % l],
-            path2['longitude'][(i) % l],
-            path2['latitude'][(i-1) % l],
-            path2['latitude'][(i) % l],
-        )
-        afterlon, afterlat = midpoint(
-            path2['longitude'][(i+1) % l],
-            path2['longitude'][(i) % l],
-            path2['latitude'][(i+1) % l],
-            path2['latitude'][(i) % l],
-        )
-        lons = [
-            beforelon,
-            path2['longitude'][i],
-            afterlon,
-        ]
-        lats = [
-            beforelat,
-            path2['latitude'][i],
-            afterlat,
-        ]
-        temp_diff = path2[year][i]
-
-        ax.plot(
-            lons,
-            lats,
-            linewidth=8,
-            color=cmap(norm(path2[year][i])),
-            transform=ccrs.Geodetic()
-        )
-
-    fig.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap),
+    fig.colorbar(plt.cm.ScalarMappable(norm=NORM, cmap=CMAP),
                  ax=ax, label='Change in SST (°C)')
 
     gl = ax.gridlines(draw_labels=True)
@@ -164,31 +163,8 @@ def year_frame(year: str, path_sst: pl.DataFrame, maxabs: float):
 def main():
     os.chdir(PROJECT_DIR)
 
-    path_sst = pl.read_csv(DATA_FILE)
-
-    # Get differences since 1982
-    path_sst = (
-        path_sst
-        .with_columns([
-            (pl.col(y) - pl.col(path_sst.columns[3])).alias(y) for y in path_sst.columns[4:]
-        ] + [pl.lit(0).alias(path_sst.columns[3])]
-        )
-    )
-    print(path_sst)
-
-    # Get absolute maximum change since 1982 - for colouring.
-    years = path_sst.columns[3:]
-    maxabs = max(
-        path_sst
-        .select([
-            pl.col(y).abs().alias(y) for y in years
-        ])
-        .max()
-        .row(0)
-    )
-
-    for year in years:
-        year_frame(year, path_sst, maxabs)
+    for year in YEARS:
+        year_frame(year)
 
 
 if __name__ == '__main__':
